2927177475368903cd77c1c631de06bc93c66c46
Ryan Mulcahy <ryan@primeacademy.io>
2016-08-04 16:19:47 -0500

    Add another test

--- test/app_test.js
+++ test/app_test.js
@@ -1,10 +1,18 @@
 var addOne = require('../src/app').addOne;
 var expect = require('chai').expect;
 
-describe('testing the addOne function', function() {
-    it('should add 1', function() {
-	var number = 42;
-	var result = addOne(number)
-	expect(result).to.equal(43);
+describe('all the tests', function(){
+  describe('testing the addOne function', function() {
+    it('should add 1 to the argument', function() {
+  	  var number = 42;
+  	  var result = addOne(number)
+  	  expect(result).to.equal(43);
     });
-});
+
+    it('adds one to the number 0', function() {
+      expect(addOne(0)).to.equal(1);
+    });
+  });
+
+  ///describe....
+})
